7bee7cd52e70ca5005b64b6d3036fdd47e3bdc86
Sanjeev <choudharysanjeev61@gmail.com>
2020-05-06 13:23:50 +0530

    SecondCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare let particlesJS: any; // Required to be properly interpreted by TypeScript.
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  public ngOnInit():void{
+    this.invokeParticles();
+  }
   title = 'particlesDemo';
+  public invokeParticles():void{
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+      
+    });
+  }
+  
 }
